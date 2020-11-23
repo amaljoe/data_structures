30cf6d02bf133567cba83bfef69e9a513503be89
amaljoe <amaljoe88@gmail.com>
2020-11-23 19:50:42 +0530

    polynomial linked list starter code

---
+++ polynomial_linkedlist/polynomial_linkedlist.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+struct node{
+    int coeff;
+    int exp;
+    struct node* link;
+};
+
+struct node p1_header;
+struct node p2_header;
+struct node a_header;
+struct node m_header;
+
+struct node* getNode();
+void add_term(int, int);
+void input(struct node*);
+void display(struct node*);
+void add();
+void multiply();
+
+void main(){
+    
+}
+
+struct node* getNode(){
+
+}
+
+void add_term(int coeff, int exp){
+
+}
+
+void input(struct node* header){
+
+}
+
+void display(struct node* header){
+
+}
+
+void add(){
+
+}
+
+void multiply(){
+    
+}
+
