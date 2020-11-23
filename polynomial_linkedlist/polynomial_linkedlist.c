9a12342d3a67b97305c8f67184153b65f4c15a34
amaljoe <amaljoe88@gmail.com>
2020-11-23 20:08:43 +0530

    intermediate

--- polynomial_linkedlist/polynomial_linkedlist.c
+++ polynomial_linkedlist/polynomial_linkedlist.c
@@ -12,7 +12,6 @@ struct node a_header;
 struct node m_header;
 
 struct node* getNode();
-void add_term(int, int);
 void input(struct node*);
 void display(struct node*);
 void add();
@@ -23,15 +22,34 @@ void main(){
 }
 
 struct node* getNode(){
-
+    return (struct node*) malloc(sizeof(struct node)); 
 }
 
-void add_term(int coeff, int exp){
-
-}
 
 void input(struct node* header){
-
+    struct node* header = getNode();
+    if(header == NULL){
+        printf("Memory not available.\n");
+        return;
+    }
+    int i, n, coeff, exp;
+    printf("Enter the no. of terms in the polynomial:\n");
+    scanf("%d", &n);
+    struct node* node = header -> link;
+    printf("Enter the co-efficient followed by exponent for each term:\n");
+    for(i = 0; i < n; i++){
+        scanf("%d", &coeff);
+        scanf("%d", &exp);
+        struct node* term = getNode();
+        if(term == NULL){
+            printf("Memory not available.\n");
+            return;
+        }
+        term -> coeff = coeff;
+        term -> exp = exp;
+        term -> link = NULL;
+        node -> link = term;
+    }
 }
 
 void display(struct node* header){
@@ -43,6 +61,6 @@ void add(){
 }
 
 void multiply(){
-    
+
 }
 
